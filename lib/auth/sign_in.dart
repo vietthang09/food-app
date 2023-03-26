039e3c388579a866a553dd8f3a0832d0dfe799d1
vietthang09 <vthcvn@gmail.com>
2023-03-26 09:46:43 +0700

    feat: build login screen

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  Future<void> _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+
+      return;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover, image: AssetImage('assets/background.png'))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Sign in to continute"),
+                    Text(
+                      "VKU",
+                      style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.white,
+                          shadows: [
+                            BoxShadow(
+                                blurRadius: 5,
+                                color: Colors.green.shade900,
+                                offset: Offset(3, 3))
+                          ]),
+                    ),
+                    Column(
+                      children: [
+                        SignInButton(
+                          Buttons.Apple,
+                          text: "Sign up with Apple",
+                          onPressed: () {},
+                        ),
+                        SignInButton(
+                          Buttons.Google,
+                          text: "Sign up with Google",
+                          onPressed: () async {
+                            await _googleSignUp().then((value) => null);
+                          },
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "By signing in you are agreeing to our",
+                          style: TextStyle(color: Colors.grey[800]),
+                        ),
+                        Text(
+                          "Terms and Privacy Policy",
+                          style: TextStyle(color: Colors.grey[800]),
+                        )
+                      ],
+                    ),
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
