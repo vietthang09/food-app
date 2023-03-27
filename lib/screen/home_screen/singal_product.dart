ec4aea567a73ce30c997a2b62e4974aba59a6eca
vietthang09 <vthcvn@gmail.com>
2023-03-27 09:02:30 +0700

    feat: build product overview screen

--- lib/screen/home_screen/singal_product.dart
+++ lib/screen/home_screen/singal_product.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class SingalProduct extends StatelessWidget {
+  final String productImage;
+  final String productName;
+  final void Function()? onTap;
+
+  SingalProduct({
+    required this.productImage,
+    required this.productName,
+    required this.onTap,
+  });
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
+      margin: EdgeInsets.only(right: 10),
       height: 230,
-      width: 160,
+      width: 165,
       decoration: BoxDecoration(
         color: Color(0xffd9dad9),
         borderRadius: BorderRadius.circular(10),
@@ -14,16 +23,24 @@ class SingalProduct extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              height: 150,
+              padding: EdgeInsets.all(5),
+              width: double.infinity,
+              child: Image.network(productImage),
+            ),
+          ),
           Expanded(
-              flex: 2, child: Image.asset('assets/fresh-fruit_vegetable.png')),
-          Expanded(
+            flex: 2,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Fresh Basil",
+                    productName,
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
